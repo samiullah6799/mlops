d96a564e6f6eb5397f18fbf3a31e77bfdc4ab151
samiullah6799 <samiulla6799@gmail.com>
2024-02-14 00:18:38 +0500

    changes in test.py

--- main.py
+++ main.py
@@ -2,11 +2,13 @@ class Wallet:
 	def __init__(self, amount):
 		self.amount = amount
 
-	def getAmount():
-		return self.amount = amount
+	def getAmount(self):
+		return self.amount
 
 	def withdraw(self, amount):
 		self.amount = self.amount - amount
+		return self.amount
 
 	def deposit(self, amount):
 		self.amount = self.amount + amount
+		return self.amount
---
+++ test.py
@@ -0,0 +1,12 @@
+from main import Wallet
+
+wallet = Wallet(2000)
+
+def test_getAmount():
+	assert wallet.getAmount() == 2000
+
+def test_deposit():
+	assert wallet.deposit(2000) == 4000
+
+def test_withdraw():
+	assert wallet.withdraw(2000) == 2000
